d461c5dadf57841375583c94551f94dfcd83e7b7
shortdaddy0711 <sodoatm@gmail.com>
2021-06-01 20:45:44 -0700

    added click event to navbar menu items

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-'use strict'
+'use strict';
 
 const navbar = document.querySelector('#navbar');
 
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log('navbar height: ', navbarHeight);
-    if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark')
-    }
+	// console.log(window.scrollY);
+	// console.log('navbar height: ', navbarHeight);
+	if (window.scrollY > navbarHeight) {
+		navbar.classList.add('navbar--dark');
+	} else {
+		navbar.classList.remove('navbar--dark');
+	}
+});
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+document.addEventListener('click', (e) => {
+	const link = e.target.dataset.link;
+
+	if (link !== undefined) {
+		const scrollTo = document.querySelector(link);
+		scrollTo.scrollIntoView({ behavior: 'smooth' });
+	}
 });
